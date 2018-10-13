c2f6f612ab0b5a5a94ca1ffbe75149cdad28a677
Mayank Aggarwal <32715597+aggarwalmayank@users.noreply.github.com>
2018-10-13 18:52:53 +0530

    update Swap

--- C/swap.c
+++ C/swap.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
+void swap(int *a,int *b)
+{
+    int c;
+    c=*a;
+    *a=*b;
+    *b=c;
+}
+    
 int main()
 {
-    int a,m,c;
+    int a,m;
     
     printf(" Enter First Number : ");
     scanf("%d" , &a);
     printf(" Enter Second Number : ");
     scanf("%d" , &m);
     
-    c=a;
-    a=m;
-    m=c;
-    
+    swap(&a,&m);
     printf(" After swapping ...\n\n");
     printf(" First Number:%d",a);
     printf("\n Second Number:%d",m);
